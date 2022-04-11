d1a06578742392f5bdc17669441635e19aeb4ed7
IkarusPrime <xxangamaxx@gmail.com>
2022-04-11 21:51:28 +0200

    Create project.

---
+++ PAC_04/src/main/java/com/example/PAC_04/Pac04Application.java
@@ -0,0 +1,13 @@
+package com.example.PAC_04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pac04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pac04Application.class, args);
+	}
+
+}
